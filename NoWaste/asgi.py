aded67105b3f2bc3cdd8c249bb55189d116a5d07
Ubuntu <golbarg.sepehrara@gmail.com>
2023-05-10 17:16:02 +0000

    refactor

--- NoWaste/asgi.py
+++ NoWaste/asgi.py
@@ -26,14 +26,4 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from chat.routing import websocket_urlpatterns
-
-application = ProtocolTypeRouter(
-    {
-        "http": application,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        ),
-    }
-)
-
+#from chat.routing import websocket_urlpattern``
--- NoWaste/wsgi.py
+++ NoWaste/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NoWaste.settings.admin")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NoWaste.settings")
 
 application = get_wsgi_application()
