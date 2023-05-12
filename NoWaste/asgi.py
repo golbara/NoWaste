95d15c751e582c37a3f9cd0e3a95b9aead10c836
Golbarg <golbarg.sepehrara@gmail.com>
2023-05-12 09:34:51 +0330

    asgi-fix-error

--- NoWaste/asgi.py
+++ NoWaste/asgi.py
@@ -9,19 +9,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NoWaste.settings")
+
+
+from django.core.asgi import get_asgi_application
+
+application = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
 from chat.routing import websocket_urlpatterns
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NoWaste.settings")
-
-application = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
