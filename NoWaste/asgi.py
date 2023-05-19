b7dcaf3e49948f474a36baaec8bc81af8cfb34c9
Golbarg <golbarg.sepehrara@gmail.com>
2023-05-19 21:53:54 +0330

    sock

--- NoWaste/asgi.py
+++ NoWaste/asgi.py
@@ -23,6 +23,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
+import chat.routing
 
 
 
