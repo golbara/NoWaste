305b29e627549b0fd51833f2262d761746b97782
Golbarg <golbarg.sepehrara@gmail.com>
2023-05-19 23:26:23 +0330

    chat

--- NoWaste/asgi.py
+++ NoWaste/asgi.py
@@ -23,15 +23,24 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
-import chat.routing
 
 
 
-application = ProtocolTypeRouter(
-    {
-        "http": application,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        ),
-    }
-)
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": application,
+#         "websocket": AllowedHostsOriginValidator(
+#             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+#         ),
+#     }
+# )
+
+application = ProtocolTypeRouter({
+    "http": application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
--- chat/routing.py
+++ chat/routing.py
@@ -1,14 +1,65 @@
-from django.urls import re_path
+# from django.urls import re_path
 
-# from . import consumers
-# "build asgi application for our web socket"
+# # from . import consumers
+# # "build asgi application for our web socket"
+# # websocket_urlpatterns = [
+# #     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+# # ]
+
+# from chat.consumers import ChatConsumer
+
 # websocket_urlpatterns = [
-#     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+#     re_path(r'ws://chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+#     # re_path(r'ws/chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 # ]
+
+
+# from django.urls import re_path
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from chat.consumers import ChatConsumer
+
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+# ]
+
+# application = ProtocolTypeRouter({
+#     'websocket': URLRouter(websocket_urlpatterns),
+# })
+from django.urls import re_path
 
 from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws://chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
-    # re_path(r'ws/chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+]
+
+from django.urls import re_path 
+ 
+# from . import consumers 
+# "build asgi application for our web socket" 
+# websocket_urlpatterns = [ 
+#     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()), 
+# ] 
+ 
+from chat.consumers import ChatConsumer 
+ 
+websocket_urlpatterns = [ 
+    re_path(r'room/(?P<room_name>\w+)/$<int:user_id>/', ChatConsumer.as_asgi()), 
+    # re_path(r'ws/chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()), 
+]
+
+
+from django.urls import re_path 
+ 
+# from . import consumers 
+# "build asgi application for our web socket" 
+# websocket_urlpatterns = [ 
+#     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()), 
+# ] 
+ 
+from chat.consumers import ChatConsumer 
+ 
+websocket_urlpatterns = [
+    re_path(r'chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 ]
