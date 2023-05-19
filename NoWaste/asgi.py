a612d905dab507a08762e06b01ca1c724b3cb097
Golbarg <golbarg.sepehrara@gmail.com>
2023-05-19 19:15:21 +0330

    socket

--- NoWaste/asgi.py
+++ NoWaste/asgi.py
@@ -17,13 +17,13 @@ from chat.routing import websocket_urlpatterns
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NoWaste.settings")
 
 django.setup()
-
 from django.core.asgi import get_asgi_application
+application = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-application = get_asgi_application()
 
 
 application = ProtocolTypeRouter(
