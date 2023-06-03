e1480c2093192b070a5704f7ec16702d165570ee
Golbarg <golbarg.sepehrara@gmail.com>
2023-06-03 17:02:54 +0330

    chat

--- NoWaste/asgi.py
+++ NoWaste/asgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 import django
 
+from django.urls import re_path 
+ 
+from chat.consumers import ChatConsumer 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NoWaste.settings")
 
 django.setup()
@@ -31,7 +35,11 @@ application = ProtocolTypeRouter(
     {
         "http": application,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            # AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter([
+                    re_path(r'chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+                ]))
+
         ),
     }
 )
--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,10 @@
 # chat/urls.py
 from django.urls import path
+
+from django.urls import re_path 
+ 
+from chat.consumers import ChatConsumer 
+
 from django.urls.conf import include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
@@ -21,5 +26,6 @@ router.register('', views.ChatViewSet, basename='chat')
 urlpatterns = [
     # path('', include(router.urls)),
     path("", views.ChatViewSet.index, name="index"),
-    path('room/<int:sender_id>/<int:reciever_id>/', views.ChatViewSet.room, name='room')
+    path('room/<int:sender_id>/<int:reciever_id>/', views.ChatViewSet.room, name='room'),
+    re_path(r'chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi())
 ]
