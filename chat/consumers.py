a07da43051d2d106ceea0ab4bfbf75f84df0d58d
Golbarg <golbarg.sepehrara@gmail.com>
2023-06-03 18:34:13 +0330

    chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,7 +8,7 @@ from User.models import *
 
 
 class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+    def connect(self,room_name):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
