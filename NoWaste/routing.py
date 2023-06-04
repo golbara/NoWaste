15aeff640193118226aa35ce85c529db94519ce2
Golbarg <golbarg.sepehrara@gmail.com>
2023-06-04 13:04:39 +0330

    chat-routing

--- NoWaste/routing.py
+++ NoWaste/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from chat.consumers import ChatConsumer 
  
 websocket_urlpatterns = [
-    re_path(r'chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'/ws/chat/room/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 ]
