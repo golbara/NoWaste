54bd61180b113d1aa0e6bbd6301083e9f5ffdc59
Golbarg <golbarg.sepehrara@gmail.com>
2023-06-06 20:28:35 +0330

    remove-sender-type

--- chat/models.py
+++ chat/models.py
@@ -47,11 +47,11 @@ from User.models import *
 
 
 class Chat(models.Model):
-    class SenderType(models.TextChoices):
-        server = 'SERVER'
-        Client = 'CLIENT'
+    # class SenderType(models.TextChoices):
+    #     server = 'SERVER'
+    #     Client = 'CLIENT'
     sender = models.ForeignKey(Customer, related_name='send_chats', on_delete=models.CASCADE)
     message = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     room_name = models.CharField(max_length=250)
-    sender_type= models.CharField(max_length=6, choices=SenderType.choices, null=True)
+    # sender_type= models.CharField(max_length=6, choices=SenderType.choices, null=True)
