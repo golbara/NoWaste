5d726c15b06838c0c49f920cca21c80fc98bf373
Ubuntu <golbarg.sepehrara@gmail.com>
2023-07-05 18:59:31 +0000

    server-migs

--- chat/admin.py
+++ chat/admin.py
@@ -9,6 +9,9 @@ from .models import *
 class ChatAdmin(admin.ModelAdmin):
     ordering = ['date_created']
     list_display = ['room_name','sender_id','reciever_id','date_created']
+   # list_display = ['room_name','date_created']
     list_filter = ['sender_id','reciever_id','date_created','room_name']
+   # search_fields = ('room_name','sender_id','reciever_id','date_created')
+   # list_filter = ['sender_id','date_created','room_name']
     search_fields = ('room_name','sender_id','reciever_id','date_created')
 admin.site.register(Chat, ChatAdmin)
